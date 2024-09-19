7ddf5822718a3f3d800d29deb2fcd394702e441a
Dorian <dorian.erhan@gmail.com>
2024-09-19 15:47:25 +0200

    Reset counter when switching tabs

--- src/background.ts
+++ src/background.ts
@@ -1,14 +1,21 @@
 let requestCount: number = 0;
+let activeTabId: number | undefined = undefined;
 
 chrome.webRequest.onCompleted.addListener(
   async (details) => {
-    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+    //const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
     requestCount++;
-    if (tab.id !== undefined) {
-      chrome.tabs.sendMessage(tab.id, {count: requestCount});
-      //chrome.tabs.sendMessage(details.tabId, {count: requestCount});
+    if (activeTabId !== undefined) {
+      chrome.tabs.sendMessage(activeTabId, {count: requestCount});
     }
-    console.log(details)
+    //console.log(details)
   },
   { urls: ["<all_urls>"] }
 )
+
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  requestCount = 0; 
+  activeTabId = activeInfo.tabId;
+
+  chrome.tabs.sendMessage(activeTabId, { count: requestCount });
+});
