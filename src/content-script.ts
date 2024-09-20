3887283e2c09289099b607c5de77e2b50abe2379
Dorian <dorian.erhan@gmail.com>
2024-09-20 18:53:03 +0200

    Initial design of extension popover

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,16 +1,26 @@
 let counterDiv: HTMLDivElement;
 
 function createCounterDiv() {
-  counterDiv = document.createElement('div');
-  counterDiv.className = 'network-request-counter';
-  document.body.appendChild(counterDiv);
+  const url = chrome.runtime.getURL('assets/html/expanded.html');
+
+  fetch(url)
+  .then(response => response.text()) 
+  .then(html => {
+    const wrapper: HTMLDivElement = document.createElement('div');
+    wrapper.className = "expanded-wrapper";
+    wrapper.innerHTML = html;
+    document.body.appendChild(wrapper);}
+  )
+  .catch(error => {
+    console.error('Error loading HTML file:', error);
+  });
 }
 
 function updateCounter(count: number) {
-  if (!counterDiv) {
-    createCounterDiv();
-  }
-  counterDiv.textContent = `Successful requests: ${count}`;
+  //if (!counterDiv) {
+  //  createCounterDiv();
+  //}
+  //counterDiv.textContent = `Successful requests: ${count}`;
 }
 
 chrome.runtime.onMessage.addListener(
