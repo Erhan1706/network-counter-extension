658b869b4f87813a2ead2341ab6925237384540f
Dorian <dorian.erhan@gmail.com>
2024-09-21 23:45:29 +0200

    Design minimal.html & show/hide minimal & disable

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,19 +1,39 @@
 let counterDiv: HTMLDivElement;
 
-function createCounterDiv() {
-  const url = chrome.runtime.getURL('assets/html/expanded.html');
+function toggleExtension(result: { disable?: boolean; minimal?: boolean }) {
+  if (!result.disable) {
+    renderExtension(result);
+  } else {
+    const injectedHTML: HTMLDivElement = document.querySelector(
+      ".extension-container"
+    ) as HTMLDivElement;
+    if (injectedHTML) document.body.removeChild(injectedHTML);
+  }
+}
+
+function renderExtension(result: { disable?: boolean; minimal?: boolean }) {
+  if (result.disable) return;
+  let url: string;
+  if (result.minimal) url = chrome.runtime.getURL("assets/html/minimal.html");
+  else url = chrome.runtime.getURL("assets/html/expanded.html");
+
+  // Clean up the previous version if it exists
+  const injectedHTML: HTMLDivElement = document.querySelector(
+    ".extension-container"
+  ) as HTMLDivElement;
+  if (injectedHTML) document.body.removeChild(injectedHTML);
 
   fetch(url)
-  .then(response => response.text()) 
-  .then(html => {
-    const wrapper: HTMLDivElement = document.createElement('div');
-    wrapper.className = "expanded-wrapper";
-    wrapper.innerHTML = html;
-    document.body.appendChild(wrapper);}
-  )
-  .catch(error => {
-    console.error('Error loading HTML file:', error);
-  });
+    .then((response) => response.text())
+    .then((html) => {
+      const wrapper: HTMLDivElement = document.createElement("div");
+      wrapper.className = "extension-container";
+      wrapper.innerHTML = html;
+      document.body.appendChild(wrapper);
+    })
+    .catch((error) => {
+      console.error("Error injecting HTML in the DOM:", error);
+    });
 }
 
 function updateCounter(count: number) {
@@ -23,12 +43,19 @@ function updateCounter(count: number) {
   //counterDiv.textContent = `Successful requests: ${count}`;
 }
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.count !== undefined) {
-      updateCounter(request.count);
-    }
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (
+    request.action === "toggle-disable" ||
+    request.action === "toggle-minimal"
+  )
+    chrome.storage.local.get(
+      { disable: false, minimal: false },
+      toggleExtension
+    );
+  if (request.count !== undefined) {
+    updateCounter(request.count);
   }
-);
+});
 
-createCounterDiv();
+// On first load of the content script, check if the extension is disabled
+chrome.storage.local.get({ disable: false, minimal: false }, toggleExtension);
--- src/popup.ts
+++ src/popup.ts
@@ -1,6 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   const disableSwitch : HTMLInputElement = document.getElementById("disable-switch") as HTMLInputElement;
+  const minimalSwitch : HTMLInputElement = document.getElementById("minimal-switch") as HTMLInputElement;
+
+  chrome.storage.local.get({ disable: false, minimal: false }, function (result) {
+    disableSwitch.checked = result.disable;
+    minimalSwitch.checked = result.minimal;
+  });
+
   disableSwitch.addEventListener("change", () => {
-    chrome.storage.sync.set({ disable: disableSwitch.checked });
+    chrome.storage.local.set({ disable: disableSwitch.checked });
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs[0]?.id !== undefined) chrome.tabs.sendMessage(tabs[0].id, { action: "toggle-disable", state: disableSwitch.checked });
+    });
+  });
+
+  minimalSwitch.addEventListener("change", () => {
+    chrome.storage.local.set({ minimal: minimalSwitch.checked });
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs[0]?.id !== undefined) chrome.tabs.sendMessage(tabs[0].id, { action: "toggle-minimal", state: minimalSwitch.checked });
+    });
   });
 });
