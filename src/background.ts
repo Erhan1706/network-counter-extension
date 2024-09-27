e3feb20a5b63ffa554c9458c85462e2fcc0284fe
Dorian <dorian.erhan@gmail.com>
2024-09-27 16:28:22 +0200

    Update service worker to increment and send different types of requests

--- src/background.ts
+++ src/background.ts
@@ -1,21 +1,95 @@
-let requestCount: number = 0;
-let activeTabId: number | undefined = undefined;
+interface RequestCount {
+  image: number;
+  stylesheet: number;
+  script: number;
+  font: number;
+  total: number;
+  error: number;
+  reset: () => void;
+}
+
+let htmlRendered: boolean = false;
+let requestCount: RequestCount = {
+  image: 0,
+  stylesheet: 0,
+  script: 0,
+  font: 0,
+  total: 0,
+  error: 0,
+  reset: function () {
+    this.image = 0;
+    this.stylesheet = 0;
+    this.script = 0;
+    this.font = 0;
+    this.total = 0;
+  }
+};
+
+function countRequest(type: chrome.webRequest.ResourceType) {
+  requestCount.total++;
+  switch (type) {
+    case "image":
+      requestCount.image++;
+      break;
+    case "stylesheet":
+      requestCount.stylesheet++;
+      break;
+    case "script":
+      requestCount.script++;
+      break;
+    case "font":
+      requestCount.font++;
+      break;
+    default:
+      break;
+  }
+}
 
 chrome.webRequest.onCompleted.addListener(
   async (details) => {
-    //const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-    requestCount++;
-    if (activeTabId !== undefined) {
-      chrome.tabs.sendMessage(activeTabId, {count: requestCount});
+    if (details.statusCode % 100 === 4 || details.statusCode % 100 === 5) {
+      requestCount.error++;
     }
-    //console.log(details)
+    countRequest(details.type);
+    if (htmlRendered) {
+      const [tab] = await chrome.tabs.query({
+        active: true,
+        lastFocusedWindow: true,
+      });
+      if (tab.id) chrome.tabs.sendMessage(tab.id, { count: requestCount });
+    }
   },
   { urls: ["<all_urls>"] }
-)
+);
 
+/* chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete") {
+    chrome.scripting.executeScript(
+      {
+        target: { tabId },
+        func: () => {
+          return true;
+        },
+      },
+      () => {
+        chrome.tabs.sendMessage(tabId, { count: requestCount });
+      }
+    );
+  }
+}); */
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "html-rendered") {
+    htmlRendered = true;
+    if (sender.tab && sender.tab.id) {
+      chrome.tabs.sendMessage(sender.tab.id, { count: requestCount });
+    }
+  }
+});
+/* 
 chrome.tabs.onActivated.addListener(async (activeInfo) => {
   requestCount = 0; 
   activeTabId = activeInfo.tabId;
 
   chrome.tabs.sendMessage(activeTabId, { count: requestCount });
-});
+}); */
--- src/content-script.ts
+++ src/content-script.ts
@@ -1,4 +1,5 @@
-let counterDiv: HTMLDivElement;
+// On first load of the content script, check if the extension is disabled
+chrome.storage.local.get({ disable: false, minimal: false }, toggleExtension);
 
 function toggleExtension(result: { disable?: boolean; minimal?: boolean }) {
   if (!result.disable) {
@@ -30,17 +31,32 @@ function renderExtension(result: { disable?: boolean; minimal?: boolean }) {
       wrapper.className = "extension-container";
       wrapper.innerHTML = html;
       document.body.appendChild(wrapper);
+      chrome.runtime.sendMessage({ action: "html-rendered" });
     })
     .catch((error) => {
       console.error("Error injecting HTML in the DOM:", error);
     });
 }
 
-function updateCounter(count: number) {
-  //if (!counterDiv) {
-  //  createCounterDiv();
-  //}
-  //counterDiv.textContent = `Successful requests: ${count}`;
+function updateCounter(count: RequestCount) {
+  const counterElement: HTMLSpanElement = document.getElementById(
+    "success-counter"
+  ) as HTMLSpanElement;
+  const cssElement: HTMLSpanElement = document.getElementById(
+    "css-counter"
+  ) as HTMLSpanElement;
+  const jsElement: HTMLSpanElement = document.getElementById(
+    "js-counter"
+  ) as HTMLSpanElement;
+  const fontElement: HTMLSpanElement = document.getElementById(
+    "font-counter"
+  ) as HTMLSpanElement;
+  const errorElement: HTMLSpanElement = document.getElementById("error-counter") as HTMLSpanElement;
+  if (counterElement) counterElement.textContent = `${count.total}`;
+  if (cssElement) cssElement.textContent = `${count.stylesheet}`;
+  if (jsElement) jsElement.textContent = `${count.script}`;
+  if (fontElement) fontElement.textContent = `${count.font}`;
+  if (errorElement) errorElement.textContent = `${count.error}`;
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
@@ -56,6 +72,3 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     updateCounter(request.count);
   }
 });
-
-// On first load of the content script, check if the extension is disabled
-chrome.storage.local.get({ disable: false, minimal: false }, toggleExtension);
